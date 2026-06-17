3a8fd57e7f8011d650bf1c5f1d073f76473f77d2
Oscar-Lilja-Simonsoft <oscar.lilja@simonsoft.se>
2026-06-17 08:48:55 +0200

    Migrate per-repository indexing test to Quarkus

---
+++ src/test/java/se/repos/indexing/config/MockableSvnDumpProfile.java
@@ -0,0 +1,19 @@
+/**
+ * Copyright (C) 2004-2012 Repos Mjukvara AB
+ */
+package se.repos.indexing.config;
+
+import java.util.Map;
+
+import io.quarkus.test.junit.QuarkusTestProfile;
+import se.simonsoft.svn.runtime.SvnDumpConfig;
+
+public class MockableSvnDumpProfile implements QuarkusTestProfile {
+
+	public static final String DEFAULT_DUMP_PATH = "se/repos/indexing/testrepo1.svndump";
+
+	@Override
+	public Map<String, String> getConfigOverrides() {
+		return Map.of(SvnDumpConfig.DUMP_PATH, DEFAULT_DUMP_PATH);
+	}
+}
