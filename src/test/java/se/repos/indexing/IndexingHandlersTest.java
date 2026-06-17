a8b6992d9e90e1c52015a104033478120518f9f7
Oscar-Lilja-Simonsoft <oscar.lilja@simonsoft.se>
2026-06-17 08:52:49 +0200

    Remove Guice from IndexingHandlersTest

--- src/test/java/se/repos/indexing/IndexingHandlersTest.java
+++ src/test/java/se/repos/indexing/IndexingHandlersTest.java
@@ -3,68 +3,70 @@
  */
 package se.repos.indexing;
 
-import static org.junit.Assert.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
-import java.util.Map;
-import java.util.Set;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 
-import se.repos.indexing.item.IndexingItemProgress;
+import se.repos.indexing.item.HandlerChecksum;
+import se.repos.indexing.item.HandlerHeadClone;
+import se.repos.indexing.item.HandlerIndexTime;
 import se.repos.indexing.item.HandlerPathinfo;
 import se.repos.indexing.item.HandlerProperties;
-
-import com.google.inject.AbstractModule;
-import com.google.inject.Binding;
-import com.google.inject.Guice;
-import com.google.inject.Injector;
-import com.google.inject.Key;
-import com.google.inject.Module;
-import com.google.inject.TypeLiteral;
-import com.google.inject.multibindings.Multibinder;
-
-import se.simonsoft.cms.item.indexing.IdStrategy;
-import se.simonsoft.cms.item.indexing.IdStrategyDefault;
+import se.repos.indexing.repository.HandlerContentDisable;
+import se.repos.indexing.repository.HandlerContentEnable;
+import se.repos.indexing.repository.IndexingItemHandlerPropertiesDisable;
+import se.repos.indexing.repository.IndexingItemHandlerPropertiesEnable;
+import se.repos.indexing.repository.MarkerRevisionComplete;
+import se.repos.indexing.scheduling.ScheduleAwaitNewer;
+import se.repos.indexing.scheduling.ScheduleBackground;
+import se.repos.indexing.solrj.HandlerSendIncrementalSolrjRepositem;
+import se.repos.indexing.solrj.HandlerSendSolrjRepositem;
+import se.repos.indexing.solrj.MarkerCommitSolrjRepositem;
 
 public class IndexingHandlersTest {
 
 	@Test
-	public void testTo() {
-		final IndexingItemHandler handler1 = new Handler("1");
-		final IndexingItemHandler handler2 = new Handler("2");
-		Module module = new AbstractModule() {
-			@Override
-			protected void configure() {
-				bind(IdStrategy.class).to(IdStrategyDefault.class);
-				Multibinder<IndexingItemHandler> multibinder = Multibinder.newSetBinder(binder(), IndexingItemHandler.class);
-				IndexingHandlers.to(multibinder, HandlerPathinfo.class, HandlerProperties.class);
-				IndexingHandlers.toInstance(multibinder, handler1, handler2);
-			}
-		};
-		Injector injector = Guice.createInjector(module);
-		Map<Key<?>, Binding<?>> allBindings = injector.getAllBindings();
-		assertTrue(allBindings.keySet().contains(Key.get(new TypeLiteral<Set<IndexingItemHandler>>(){})));
-		assertTrue(allBindings.keySet().contains(Key.get(HandlerPathinfo.class)));
-		assertTrue(allBindings.keySet().contains(Key.get(HandlerProperties.class)));
-		Set<IndexingItemHandler> handlers = injector.getInstance(Key.get(new TypeLiteral<Set<IndexingItemHandler>>(){}));
-		assertTrue(handlers.contains(handler1));
-		assertTrue(handlers.contains(handler2));
-	}
-	
-	private static class Handler implements IndexingItemHandler {
-		
-		Handler(String string) {
-		}
-
-		@Override
-		public void handle(IndexingItemProgress progress) {
-		}
-
-		@Override
-		public Set<Class<? extends IndexingItemHandler>> getDependencies() {
-			return null;
-		}
-		
+	public void testStandardHandlerOrder() {
+		assertEquals(expectedHandlerTypes(), standardHandlerTypes());
 	}
 
+	private List<Class<? extends IndexingItemHandler>> standardHandlerTypes() {
+		List<Class<? extends IndexingItemHandler>> result = new ArrayList<>();
+		add(result, IndexingHandlers.STANDARD.get(IndexingHandlers.Group.Unblock));
+		add(result, IndexingHandlers.STANDARD.get(IndexingHandlers.Group.Structure));
+		add(result, IndexingHandlers.STANDARD.get(IndexingHandlers.Group.Fast));
+		add(result, IndexingHandlers.STANDARD.get(IndexingHandlers.Group.Nice));
+		add(result, IndexingHandlers.STANDARD.get(IndexingHandlers.Group.Content));
+		add(result, IndexingHandlers.STANDARD.get(IndexingHandlers.Group.Final));
+		return result;
+	}
+
+	private void add(List<Class<? extends IndexingItemHandler>> target, Iterable<Class<? extends IndexingItemHandler>> source) {
+		for (Class<? extends IndexingItemHandler> handlerType : source) {
+			target.add(handlerType);
+		}
+	}
+
+	private List<Class<? extends IndexingItemHandler>> expectedHandlerTypes() {
+		return Arrays.asList(
+				ScheduleBackground.class,
+				HandlerIndexTime.class,
+				HandlerPathinfo.class,
+				IndexingItemHandlerPropertiesEnable.class,
+				HandlerProperties.class,
+				IndexingItemHandlerPropertiesDisable.class,
+				HandlerSendIncrementalSolrjRepositem.class,
+				ScheduleAwaitNewer.class,
+				HandlerContentEnable.class,
+				HandlerChecksum.class,
+				HandlerContentDisable.class,
+				HandlerHeadClone.class,
+				HandlerSendSolrjRepositem.class,
+				MarkerRevisionComplete.class,
+				MarkerCommitSolrjRepositem.class);
+	}
 }
